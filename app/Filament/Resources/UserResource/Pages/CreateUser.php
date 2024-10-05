52096bc368744420002932ba2aa07c4aa499d956
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2024-10-05 01:48:48 +0000

    Oct 5, 2024, 7:18 AM

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -5,33 +5,30 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Str;
-
-
+use Illuminate\Support\Facades\Hash;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
     protected $plainPassword;
 
-    protected function afterCreate(): void
-    {
-        $this->resource::afterCreate($this->record);
-    }
-
-
-    
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-    
-        $this->plainPassword = Str::random(12);
+        $this->plainPassword = Str::random(rand(12,36));
+        $data['password'] = Hash::make($this->plainPassword);
+        
+        // Store the plain password temporarily
+        $this->plainPassword = $this->plainPassword;
 
-    
-        $data['password'] = $this->plainPassword ;
-     
         return $data;
     }
 
+    protected function afterCreate(): void
+    {
+        // Access the created user through $this->record
+        $user = $this->record;
 
-
-
+        // Send invitation notification
+        $user->notify(new \App\Notifications\UserInvitationNotification($this->plainPassword));
+    }
 }
