977dff0cee1c037102e68c4acde39c4e7c8db6c6
Prasanjith Chanaka Dissanayake <59183950+pc-dissanayake@users.noreply.github.com>
2024-10-05 01:46:38 +0000

    Oct 5, 2024, 7:16 AM

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -4,13 +4,34 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Str;
+
+
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+    protected $plainPassword;
 
     protected function afterCreate(): void
     {
         $this->resource::afterCreate($this->record);
     }
+
+
+    
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+    
+        $this->plainPassword = Str::random(12);
+
+    
+        $data['password'] = $this->plainPassword ;
+     
+        return $data;
+    }
+
+
+
+
 }
